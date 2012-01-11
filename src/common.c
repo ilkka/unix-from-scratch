657ee8986b1314fb7f8015996f0eccbf05a2ebdc
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-01-11 11:29:22 +0200

    Implement memset and memcpy (..I think)

--- src/common.c
+++ src/common.c
@@ -21,3 +21,42 @@ u16int inw(u16int port)
 	asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
 	return ret;
 }
+
+void *memset(void *s, u8int c, u32int n)
+{
+	if (n > 0)
+	{
+		asm volatile (
+				".L_MEMSET_LOOP:"
+				"mov %[ch],(%[ptr]);"
+				"inc %[ptr];"
+				"dec %[count];"
+				"jnz .L_MEMSET_LOOP"
+				 :
+				 : [count] "c" (n), [ch] "r" (c), [ptr] "r" (s) 
+			     );
+	}
+	return s;
+}
+
+void *memcpy(void *dest, void *src, u32int n)
+{
+	if (n > 0)
+	{
+		asm volatile (
+				".L_MEMCPY_LOOP:"
+				"mov (%[s]),%%eax;"
+				"inc %[s];"
+				"mov %%eax,(%[d]);"
+				"inc %[d];"
+				"dec %[count];"
+				"jnz .L_MEMCPY_LOOP"
+				:
+				: [count] "c" (n), [s] "r" (src), [d] "r" (dest)
+				: "%eax"
+			     );
+	}
+	return dest;
+}
+
+
--- src/common.h
+++ src/common.h
@@ -13,9 +13,49 @@ typedef             short   s16int;
 typedef unsigned    char    u8int;
 typedef             char    s8int;
 
+/**
+ * Output byte to port.
+ *
+ * @param port the port.
+ * @param value the byte.
+ */
 void outb(u16int port, u8int value);
+
+/**
+ * Input byte from port.
+ *
+ * @param port the port.
+ * @return the byte.
+ */
 u8int inb(u16int port);
+
+/**
+ * Input word from port.
+ *
+ * @param port the port.
+ * @return the word.
+ */
 u16int inw(u16int port);
+
+/**
+ * Fill area of memory with constant byte.
+ *
+ * @param s pointer to filled memory.
+ * @param c byte to write.
+ * @param n number of bytes to write.
+ * @return pointer to s.
+ */
+void *memset(void *s, u8int c, u32int n);
+
+/**
+ * Copy memory from one area to another.
+ *
+ * @param dest pointer to destination.
+ * @param src pointer to source.
+ * @param n number of bytes to copy.
+ * @return pointer to dest.
+ */
+void *memcpy(void *dest, void *src, u32int n);
 
 #endif // COMMON_H
 
