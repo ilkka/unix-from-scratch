29b59a66be541bf6145a2553be76c95d15cd84b5
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-01-13 22:40:19 +0200

    Test printf in main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 int main(struct multiboot *mbootptr)
 {
 	monitor_clear();
-	monitor_write("Hello world!");
+	monitor_write("before printf");
+	printf("Hello %s!", "world");
+	monitor_write("after printf");
 	return 0xDEADBABA;
 }
