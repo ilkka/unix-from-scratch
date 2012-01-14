69b3f10b3251a8f8016899f1f22429c4723ea51d
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-01-15 00:52:34 +0200

    Send some interrupts

--- src/main.c
+++ src/main.c
@@ -3,5 +3,7 @@ int main(struct multiboot *mbootptr)
 	monitor_clear();
 	printf("Hello %s!\n", "world");
 	printf("Hello %d\n", 42);
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 	return 0xDEADBABA;
 }
